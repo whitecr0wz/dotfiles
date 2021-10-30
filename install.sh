ba6b19e1d5a3496536b45366259883d01995c37f
Felipe Winsnes <45699918+whitecr0wz@users.noreply.github.com>
2021-10-30 10:34:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,4 @@ dpkg -i /tmp/dotfiles/ttf-mscorefonts-installer_3.8_all.deb ; apt install -f -y
 cp -rf /tmp/dotfiles/xfce4 /root/.config/xfce4
 apt install xfce4 xfce4-panel xfce4-settings xfce4-power-manager xfce4-session xfdesktop4 xfwm4 xfce4-goodies lightdm -y
 cp /tmp/dotfiles/dragon.jpg /usr/share/backgrounds/xfce/dragon.jpg
-systemctl enable lightdm
 systemctl start lightdm
